6de9fe3faefe5e1574e7f5afd1dbb20b6c495984
caso <matteocasonato@protonmail.com>
2025-02-26 12:53:36 +0100

    hardcode write params

--- app/api/pinata/route.ts
+++ app/api/pinata/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse, type NextRequest } from "next/server";
-import { pinata } from "../../../lib/pinata"
+import { pinata } from "../../../lib/pinata";
 
 export async function POST(request: NextRequest) {
   try {
-    console.log(process.env.PINATA_JWT);
     const data = await request.formData();
     const file: File | null = data.get("file") as unknown as File;
 
@@ -11,12 +10,15 @@ export async function POST(request: NextRequest) {
     if (!file) {
       return NextResponse.json({ error: "No file provided" }, { status: 400 });
     }
-    
+
     const uploadData = await pinata.upload.file(file);
     const url = `https://gateway.pinata.cloud/ipfs/${uploadData.IpfsHash}`;
     return NextResponse.json({ url }, { status: 200 });
   } catch (e) {
     console.error(e);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
