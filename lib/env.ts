3c415524664f0d068ab89a7dc04e5e98bd6ba156
caso <matteocasonato@protonmail.com>
2025-04-24 14:21:12 +0200

    dynamic manifest

--- lib/env.ts
+++ lib/env.ts
@@ -27,6 +27,9 @@ export const env = createEnv({
     NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
     NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1),
     NEXT_PUBLIC_POSTHOG_DISABLED: z.string().optional(),
+    NEXT_PUBLIC_FARCASTER_HEADER: z.string().min(1),
+    NEXT_PUBLIC_FARCASTER_PAYLOAD: z.string().min(1),
+    NEXT_PUBLIC_FARCASTER_SIGNATURE: z.string().min(1),
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
@@ -39,5 +42,9 @@ export const env = createEnv({
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     NEXT_PUBLIC_POSTHOG_DISABLED: process.env.NEXT_PUBLIC_POSTHOG_DISABLED,
+    NEXT_PUBLIC_FARCASTER_HEADER: process.env.NEXT_PUBLIC_FARCASTER_HEADER,
+    NEXT_PUBLIC_FARCASTER_PAYLOAD: process.env.NEXT_PUBLIC_FARCASTER_PAYLOAD,
+    NEXT_PUBLIC_FARCASTER_SIGNATURE:
+      process.env.NEXT_PUBLIC_FARCASTER_SIGNATURE,
   },
 });
---
+++ lib/farcaster.ts
@@ -0,0 +1,53 @@
+import { env } from "@/lib/env";
+
+/**
+ * Get the farcaster manifest for the frame, generate yours from Warpcast Mobile
+ *  On your phone to Settings > Developer > Domains > insert website hostname > Generate domain manifest
+ * @returns The farcaster manifest for the frame
+ */
+export async function getFarcasterManifest() {
+  let frameName = "Acid Test";
+  const appUrl = env.NEXT_PUBLIC_URL;
+  if (appUrl.includes("localhost")) {
+    frameName += " Local";
+  } else if (appUrl.includes("ngrok")) {
+    frameName += " NGROK";
+  }
+  // else if (appUrl.includes("dev.acidtest.xyz")) {
+  //   frameName += " Dev";
+  // }
+  return {
+    accountAssociation: {
+      header: env.NEXT_PUBLIC_FARCASTER_HEADER,
+      payload: env.NEXT_PUBLIC_FARCASTER_PAYLOAD,
+      signature: env.NEXT_PUBLIC_FARCASTER_SIGNATURE,
+    },
+    frame: {
+      version: "1",
+      name: frameName,
+      iconUrl: `${appUrl}/images/icon.png`,
+      homeUrl: appUrl,
+      imageUrl: `${appUrl}/images/feed.jpg`,
+      buttonTitle: `Start Acid Test`,
+      splashImageUrl: `${appUrl}/images/icon.png`,
+      splashBackgroundColor: "#000000",
+      webhookUrl: `${appUrl}/api/webhook`,
+      // Metadata https://github.com/farcasterxyz/miniapps/discussions/191
+      subtitle: "", // 30 characters, no emojis or special characters, short description under app name
+      description: "", // 170 characters, no emojis or special characters, promotional message displayed on Mini App Page
+      primaryCategory: "music",
+      tags: ["music", "acid", "song"], // up to 5 tags, filtering/search tags
+      tagline: "", // 30 characters, marketing tagline should be punchy and descriptive
+      ogTitle: "", // 30 characters, app name + short tag, Title case, no emojis
+      ogDescription: "", // 100 characters, summarize core benefits in 1-2 lines
+      // screenshotUrls: [
+      //   // 1284 x 2778, visual previews of the app, max 3 screenshots
+      //   `${env.NEXT_PUBLIC_URL}/images/screenshots/1.png`,
+      //   `${env.NEXT_PUBLIC_URL}/images/screenshots/2.png`,
+      //   `${env.NEXT_PUBLIC_URL}/images/screenshots/3.png`,
+      // ],
+      // heroImageUrl: `${env.NEXT_PUBLIC_URL}/images/hero.jpg`, // 1200 x 630px (1.91:1), promotional display image on top of the mini app store
+      // ogImageUrl: `${env.NEXT_PUBLIC_URL}/images/hero.jpg`, // 1200 x 630px (1.91:1), promotional image, same as app hero image
+    },
+  };
+}
